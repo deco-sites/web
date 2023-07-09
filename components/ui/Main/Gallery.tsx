7825eadfc478347897773d1a7c86885a86fbdf82
devartes <anacavalcante731@gmail.com>
2023-07-09 14:34:43 -0300

    refactor(Gallery.tsx): remove carousel and modify structure

--- components/ui/Main/Gallery.tsx
+++ components/ui/Main/Gallery.tsx
@@ -1,8 +1,6 @@
 import type { Video as LiveImage } from "deco-sites/std/components/types.ts";
-import { useEffect, useRef, useState } from "preact/hooks";
 
-export interface Slide {
-  id?: number;
+export interface Props {
   preview?: LiveImage;
   title?: string;
   fontSizeTitle?: string;
@@ -46,13 +44,7 @@ export interface Slide {
     | "unset";
 }
 
-export interface Props {
-  slides: Slide[];
-}
-
 export default function Gallery(props: Props) {
-  const { slides } = props;
-  const [currentSlide, setCurrentSlide] = useState(0);
   const gradientStyle = {
     backgroundImage:
       "linear-gradient(to bottom, #64EF74, #59eead 50%, #0a1a13 50%, #0a1a13 100%, #0a1a13 100%)",
@@ -63,7 +55,7 @@ export default function Gallery(props: Props) {
     fontWeightTitle = "normal",
     fontSizeParagraph = "",
     fontWeightParagraph = "normal",
-  } = props.slides[currentSlide] || {};
+  } = props|| {};
 
   const titleStyle = {
     fontSize: fontSizeTitle,
@@ -75,91 +67,48 @@ export default function Gallery(props: Props) {
     fontWeight: fontWeightParagraph,
   };
 
-  useEffect(() => {
-    const timer = setTimeout(() => {
-      setCurrentSlide((prevSlide) => (prevSlide + 1) % slides.length);
-    }, 10000);
-    return () => clearTimeout(timer);
-  }, [currentSlide, slides.length]);
-
-  const goToPreviousSlide = () => {
-    setCurrentSlide((prevSlide) =>
-      (prevSlide - 1 + slides.length) % slides.length
-    );
-  };
-
-  const goToNextSlide = () => {
-    setCurrentSlide((prevSlide) => (prevSlide + 1) % slides.length);
-  };
-
   return (
     <div
       class="w-full lg:min-h-[770px] flex items-center justify-center mt-[50px]"
       style={gradientStyle}
     >
-      <div class="relative overflow-hidden lg:my-0 my-[7%] mx-[7%]">
-        <div
-          class="h-full flex transition-transform duration-1000"
-          style={`transform: translateX(-${currentSlide * 100}%)`}
-        >
-          {slides.map((slide, index) => (
-            <div
-              key={`${slide.id}-${index}`}
-              class="w-full flex-shrink-0 flex items-stretch">
-            
-              <div class="max-w-[1440px] mx-auto w-full md:py-[80px] py-[40px] rounded-md glassmorphism">
-                <div class="flex md:flex-row flex-col md:justify-between justify-center items-center md:px-[80px] px-[35px] gap-[20px]">
-                  <div class="lg:w-[30%] md:w-[50%] w-full flex md:justify-start justify-center">
-                    <div class="aspect-w-16 aspect-h-9 overflow-hidden max-w-[300px] h-full relative rounded-[13px] shadow-[3px_1px_19px_9px_#00000024]">
-                      <video
-                        src={slide.preview || ""}
-                        alt={slide.preview || ""}
-                        autoPlay
-                        muted
-                        loop
-                        preload="auto"
-                        webkit-playsinline
-                        x5-playsinline
-                        playsInline
-                        class="w-full rounded-[13px]"
-                      >
-                        Video not supported!
-                      </video>
-                    </div>
-                  </div>
-                  <div class="lg:w-[70%] md:w-[50%] w-full flex flex-col items-start gap-[20px]">
-                    <h2
-                      style={titleStyle}
-                    >
-                      <span class="bg-[#00ff5e]">{slide.title || ""}</span>
-                    </h2>
-                    <p
-                      style={paragraphStyle}
-                      class="text-white"
-                    >
-                      {slide.paragraph || ""}
-                    </p>
-                  </div>
-                </div>
+      <div class="relative lg:my-0 m-[5%] w-full h-full">
+        <div class="max-w-[1440px] mx-auto w-full h-full md:py-[20px] py-[10px] rounded-md glassmorphism">
+          <div class="flex md:flex-row flex-col md:justify-between justify-center items-center px-[20px] gap-[40px] w-full h-full">
+            <div class="md:w-[50%] w-full flex md:justify-start justify-center">
+              <div class="aspect-w-16 aspect-h-9 w-full h-full relative rounded-lg shadow-[3px_1px_19px_9px_#00000024]">
+                <video
+                  src={props.preview || ""}
+                  alt={props.preview || ""}
+                  autoPlay
+                  muted
+                  loop
+                  preload="auto"
+                  webkit-playsinline
+                  x5-playsinline
+                  playsInline
+                  class="w-full rounded-lg relative z-[-1]"
+                >
+                  Video not supported!
+                </video>
               </div>
             </div>
-          ))}
-        </div>
-        <div class="absolute top-[45%] left-0 right-0 flex justify-between">
-          <button
-            class="text-3xl bg-[#00ff5e] text-[#0a1a13] py-3 px-2 focus:outline-none"
-            onClick={goToPreviousSlide}
-            aria-label="button previous slide"
-          >
-            &#8249;
-          </button>
-          <button
-            class="text-3xl bg-[#00ff5e] text-[#0a1a13] py-3 px-2 focus:outline-none"
-            onClick={goToNextSlide}
-            aria-label="button next slide"
-          >
-            &#8250;
-          </button>
+            <div class="md:w-[50%] w-full flex flex-col items-start gap-[20px]">
+              <h2
+                style={titleStyle}
+              >
+                <span class="bg-[#00ff5e]">
+                  {props.title || ""}
+                </span>
+              </h2>
+              <p
+                style={paragraphStyle}
+                class="text-white"
+              >
+                {props.paragraph || ""}
+              </p>
+            </div>
+          </div>
         </div>
       </div>
     </div>
--- live.gen.ts
+++ live.gen.ts
@@ -6,9 +6,8 @@ import { DecoManifest } from "$live/types.ts";
 import * as $0 from "./functions/LoadGitHubRaw.ts";
 import * as $$$$0 from "./routes/styles.css.ts";
 import * as $$$$1 from "./routes/_app.tsx";
-import * as $$$$$0 from "./islands/Gallery.tsx";
-import * as $$$$$1 from "./islands/Main.tsx";
-import * as $$$$$2 from "./islands/Nav.tsx";
+import * as $$$$$0 from "./islands/Main.tsx";
+import * as $$$$$1 from "./islands/Nav.tsx";
 import * as $$$$$$$$0 from "./sections/DesignSystem.tsx";
 import * as $$$$$$$$1 from "./sections/Footer.tsx";
 import * as $$$$$$$$2 from "./sections/Gallery.tsx";
@@ -190,9 +189,8 @@ const manifest = {
     "./routes/styles.css.ts": $$$$0,
   },
   "islands": {
-    "./islands/Gallery.tsx": $$$$$0,
-    "./islands/Main.tsx": $$$$$1,
-    "./islands/Nav.tsx": $$$$$2,
+    "./islands/Main.tsx": $$$$$0,
+    "./islands/Nav.tsx": $$$$$1,
   },
   "sections": {
     "$live/sections/Conditional_Beta.tsx": i2$$$$$0,
