4cfef68485091b76040db18c157532e836aca290
Ana Carolina D. C <76822093+devartes@users.noreply.github.com>
2023-09-28 10:58:27 -0300

    refactor: improving styles and logic for mobile version

--- sections/Search.tsx
+++ sections/Search.tsx
@@ -1,6 +1,6 @@
 import type { Image as LiveImage } from "deco-sites/std/components/types.ts";
 import Image from "deco-sites/std/components/Image.tsx";
-import { useState } from "preact/hooks";
+import { useEffect, useState } from "preact/hooks";
 
 export interface SearchProps {
   placeholder?: string;
@@ -18,72 +18,96 @@ export interface Links {
 export default function Search(props: SearchProps) {
   const { placeholder, icon, links } = props;
   const [showMenu, setShowMenu] = useState(false);
+  const [isMobile, setIsMobile] = useState(window.innerWidth <= 768);
 
   const toggleMenu = () => {
     setShowMenu(!showMenu);
   };
 
+  useEffect(() => {
+    const handleResize = () => {
+      setIsMobile(window.innerWidth <= 768);
+    };
+
+    addEventListener("resize", handleResize);
+
+    return () => {
+      removeEventListener("resize", handleResize);
+    };
+  }, []);
+
   const createPlaceholderStructure = () => {
     return (
-      <div class="flex items-center justify-center relative">
-        <div class="flex items-center justify-center relative w-[95%]">
+      <ul class="flex h-full items-center justify-center relative">
+        <li class="flex items-center justify-center relative w-[95%]">
           <input
             type="text"
-            class="bg-[#c9f888] focus:outline-none h-[40px] lg:ml-[20px] my-[5px] pl-[15px] placeholder-[#333] pr-[40px] py-[5px] rounded-[30px] shadow-box-shadow-1 text-[#333] w-full"
+            class="bg-[#053535] focus:outline-none h-[40px] lg:ml-[20px] md:bg-[#c9f888] md:my-[5px] md:placeholder-[#333] py-[5px] md:text-[#333] mt-[5px] pl-[15px] placeholder-white pr-[40px] pt-[5px] rounded-[30px] shadow-box-shadow-1 text-white w-full"
             placeholder={placeholder}
           />
           <div class="absolute bg-[#053535] flex h-[30px] items-center justify-center p-[5px] right-[5px] rounded-full w-[30px]">
-            <Image src={icon || ""} width={20} height={20} alt="icon" />
+            <Image src={icon || ""} width={20} height={20} alt="icon search" />
           </div>
-        </div>
-        <div class="hidden lg:block w-[5%]">
+        </li>
+        <span class="hidden lg:block w-[5%]">
           <hr class=" border-[#00ff80] h-full relative" />
-        </div>
-      </div>
+        </span>
+      </ul>
     );
   };
 
   const createLinksStructure = () => {
     return (
-      <div class="bg-[#053535] flex items-center justify-center">
-        <div class="w-1/5">
+      <ul class="flex flex-col h-full items-center justify-center md:bg-[#053535] md:flex-row">
+        <span class="hidden md:block w-1/5">
           <hr class="border-[#00ff80] h-full relative" />
-        </div>
-        <div class="w-4/5">
-          <ul class="bg-[#00ff80] flex flex-wrap gap-x-[40px] gap-y-[10px] h-[40px] items-center justify-center relative rounded-[30px] shadow-box-shadow-1">
+        </span>
+        <li class="md:w-4/5 w-full">
+          <ul class="bg-[#00ff80] flex flex-col flex-wrap gap-x-[40px] md:gap-y-[10px] h-auto items-center justify-center md:flex-row md:h-[40px] md:shadow-box-shadow-1 relative rounded-[30px]">
             {links?.slice(0, 3).map((link, index) => (
-              <li key={index}>
+              <li
+                key={index}
+                class="border-[#053535] border-b flex justify-center md:border-0 md:justify-start md:w-auto w-full"
+              >
                 <a
                   href={link.url}
-                  class="font-semibold hover:border-[#053535] hover:border-b-4 pb-[6px] text-[#333] text-[1em]"
+                  class="duration-300 ease-in font-semibold hover:border-[#053535] md:hover:border-b-4 md:text-[1em] py-[6px] text-[#333] text-[1.2em]"
                 >
                   {link.label}
                 </a>
               </li>
             ))}
           </ul>
-        </div>
-        <div class="w-1/5">
+        </li>
+        <span class="hidden md:block w-1/5">
           <hr class=" border-[#00ff80] h-full relative" />
-        </div>
-      </div>
+        </span>
+      </ul>
     );
   };
 
   return (
     <div class="bg-[#053535]">
       <div class="grid grid-cols-1 lg:grid-cols-3 max-w-[1440px] mx-auto">
-        {createPlaceholderStructure()}
-        {createLinksStructure()}
+        <>
+          {!isMobile
+            ? (
+              <li>
+                {createPlaceholderStructure()}
+              </li>
+            )
+            : null}
+        </>
+        <>{!isMobile ? <li>{createLinksStructure()}</li> : null}</>
         <ul class="bg-[#053535] flex flex-col flex-wrap gap-x-[40px] gap-y-[10px] items-center relative">
           {links && links.length > 3 && (
             <li class="flex items-center justify-center lg:justify-start my-[5px] w-full">
               <span class="hidden lg:block w-2/5">
                 <hr class="border-[#00ff80] h-full relative" />
               </span>
-              <div class="bg-[#00ff80] lg:w-1/5 rounded-[30px]">
+              <div class="bg-[#00ff80] lg:w-1/5 p-[5px] rounded-[30px] w-1/2">
                 <button
-                  class="bg-[#053535] flex font-semibold gap-[10px] items-center m-[5px] px-[10px] py-[5px] rounded-[30px] text-[1em] text-white"
+                  class="bg-[#053535] flex font-semibold gap-[10px] items-center justify-center md:text-[1em] px-[10px] py-[5px] rounded-[30px] text-[1.2em] text-white w-full"
                   onClick={toggleMenu}
                 >
                   {props.button}
@@ -98,18 +122,34 @@ export default function Search(props: SearchProps) {
             </li>
           )}
           {showMenu && (
-            <ul class="absolute bg-[#00ff80] flex flex-col gap-y-[10px] items-center justify-center lg:max-w-[300px] lg:top-[54px] min-w-[200px] p-[10px] rounded-b-[20px] shadow-box-shadow-2 top-[45px] w-full z-20">
-              {links?.slice(3).map((link) => (
-                <li class="border-[#053535] border-b flex justify-center pb-[5px] w-full">
-                  <a
-                    href={link.url}
-                    class="duration-300 ease-in font-semibold hover:scale-110 text-[#333] text-[1em]"
-                  >
-                    {link.label}
-                  </a>
-                </li>
-              ))}
-            </ul>
+            <>
+              <ul class="absolute bg-[#00ff80] flex flex-col md:gap-y-[10px] items-center justify-center lg:max-w-[300px] lg:top-[54px] md:top-[45px] min-w-[200px] p-[10px] md:rounded-b-[20px] shadow-box-shadow-2 top-[52px] w-full z-20">
+                <>
+                  {isMobile
+                    ? (
+                      <li class="w-full">
+                        {createPlaceholderStructure()}
+                      </li>
+                    )
+                    : null}
+                </>
+                <>
+                  {isMobile
+                    ? <li class="w-full">{createLinksStructure()}</li>
+                    : null}
+                </>
+                {links?.slice(3).map((link) => (
+                  <li class="border-[#053535] border-b flex justify-center w-full">
+                    <a
+                      href={link.url}
+                      class="duration-300 ease-in font-semibold md:hover:scale-110 md:text-[1em] text-[#333] text-[1.2em] py-[6px]"
+                    >
+                      {link.label}
+                    </a>
+                  </li>
+                ))}
+              </ul>
+            </>
           )}
         </ul>
       </div>
