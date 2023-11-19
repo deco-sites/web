b22b12a8b58a5c5e574943886fbc7e291204c19d
Ana Carolina D. C <76822093+devartes@users.noreply.github.com>
2023-11-19 14:24:48 -0300

    refactor: testing font performance

--- sections/FontText.tsx
+++ sections/FontText.tsx
@@ -1,20 +1,19 @@
 export interface FontsProps {
-    font1: string;
-    font2: string;
-    font3: string;
-    font4: string;
+  font1: string;
+  font2: string;
+  font3: string;
+  font4: string;
 }
 
-export default function Fonts({ font1, font2, font3 , font4 }: FontsProps) {
-    return (
-        <div className="bg-[#fbffc8]">
-            <div className="flex flex-col max-w-[1440px] mx-auto gap-5">
-                <p class="font1 text-black font-bold text-4xl">{`${font1}`}</p>
-                <p class="font2 text-black font-bold text-4xl">{`${font2}`}</p>
-                <p class="font3 text-black font-bold text-4xl">{`${font3}`}</p>
-                <p class="font4 text-black font-bold text-4xl">{`${font4}`}</p>
-            </div>
-        </div>
-    );
+export default function Fonts({ font1, font2, font3, font4 }: FontsProps) {
+  return (
+    <div className="bg-[#fbffc8]">
+      <div className="flex flex-col max-w-[1440px] mx-auto gap-5">
+        <p class="font-bold font1 text-4xl text-black">{`${font1}`}</p>
+        <p class="font-bold font2 text-4xl text-black">{`${font2}`}</p>
+        <p class="font-bold font3 text-4xl text-black">{`${font3}`}</p>
+        <p class="font-bold font4 text-4xl text-black">{`${font4}`}</p>
+      </div>
+    </div>
+  );
 }
-
